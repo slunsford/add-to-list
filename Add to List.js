fdbc5880bb02d076a2d4b15466d3926fb0beb0c4
Sean Lunsford <sean@thedarkroast.com>
2018-05-05 12:00:54 -0400

    Clean up
    Move non-function code to the top and flesh out comments

--- Add to List.js
+++ Add to List.js
@@ -1,9 +1,27 @@
 // Add to List
 
-// User values
-const listTag = "lists"
-const defaultBullet = "-"
-const defaultChecklist = "- [ ]"
+// User values (editable)
+const defaultBullet = "-"				// Alternatives: * or +
+const defaultChecklist = "- [ ]"		// Alternatives: "* [ ]" or "+ [ ]"
+const listTag = "lists"					// Alternatives: ¯\_(ツ)_/¯ (whatever you want it to be)
+
+// (Don't edit these)
+const bulletRegex = /^([-*+]( \[( |x)\])?|\d+\.)/gm
+var bullet = ""
+var d = selectDraft()
+
+if (d) {
+	if (bullet == "") {  // If bullet not already set (i.e. while creating a new list)
+		// Get last bullet used (last matters for numbered list)
+		bullet = d.content.match(bulletRegex).reverse()[0]
+	}
+	var lines = draft.content.replace(bulletRegex,"").split("\n")  // Strip existing bullets and split to array
+	d.content += "\n" + lines.map(addBullet).join("\n")  // Add bullet and append to draft
+	d.update()
+}
+else {
+	context.cancel()
+}
 
 var selectDraft = function() {
 	var drafts = Draft.query("", "inbox", [listTag]).reverse()  // Get all list drafts newest to oldest
@@ -71,7 +89,7 @@ var newList = function() {
 }
 
 var addBullet = function(text, index) {
-	n = parseFloat(bullet)			// Get numeral if numbered list
+	var n = parseFloat(bullet)		// Get numeral if numbered list
 	if (n || bullet == "0.") {		// Include condition where new numbered list created
 		n += index + 1  				// Iterate numeral by one
 		return n + ". " + text.trim()
@@ -79,23 +97,4 @@ var addBullet = function(text, index) {
 	else {
 		return bullet.replace("[x]","[ ]") + " " + text.trim()  // Replace checked box with empty box
 	}
-}
-
-const bulletRegex = /^([-*+]( \[( |x)\])?|\d+\.)/gm
-var bullet = ""
-var n = 0
-
-var d = selectDraft()
-
-if (d) {
-	if (bullet == "") {  // If bullet not already set (i.e. while creating a new list)
-		// Get last bullet used (last matters for numbered list)
-		bullet = d.content.match(bulletRegex).reverse()[0]
-	}
-	var lines = draft.content.replace(bulletRegex,"").split("\n")  // Strip existing bullets and split to array
-	d.content += "\n" + lines.map(addBullet).join("\n")  // Add bullet and append to draft
-	d.update()
-}
-else {
-	context.cancel()
 }
