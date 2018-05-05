1329a7038381662f1a261ae7f359b57bc0b4a00d
Sean Lunsford <sean@thedarkroast.com>
2018-05-05 12:59:23 -0400

    Move list type selection to new selectListType() function
    Call if no bullet found instead of during newList(). Also move
    functions back to top.

--- Add to List.js
+++ Add to List.js
@@ -5,24 +5,6 @@ const defaultBullet = "-"				// Alternatives: * or +
 const defaultChecklist = "- [ ]"		// Alternatives: "* [ ]" or "+ [ ]"
 const listTag = "lists"					// Alternatives: ¯\_(ツ)_/¯ (whatever you want it to be)
 
-// (Don't edit these)
-const bulletRegex = /^([-*+]( \[( |x)\])?|\d+\.)/gm
-var bullet = ""
-var d = selectDraft()
-
-if (d) {
-	if (bullet == "") {  // If bullet not already set (i.e. while creating a new list)
-		// Get last bullet used (last matters for numbered list)
-		bullet = d.content.match(bulletRegex).reverse()[0]
-	}
-	var lines = draft.content.replace(bulletRegex,"").split("\n")  // Strip existing bullets and split to array
-	d.content += "\n" + lines.map(addBullet).join("\n")  // Add bullet and append to draft
-	d.update()
-}
-else {
-	context.cancel()
-}
-
 var selectDraft = function() {
 	var drafts = Draft.query("", "inbox", [listTag]).reverse()  // Get all list drafts newest to oldest
 	var titles = drafts.map(function(d) { return d.title })  // Get titles of all drafts
@@ -58,30 +40,31 @@ var newList = function() {
 	var didCreate = p.show()
 	
 	if (didCreate) {
-		var title = p.fieldValues["title"]
-		
-		// Prompt for list style
-		p = Prompt.create()
-		p.title = "Select List Type"
-		p.addButton(defaultBullet + " Bulleted")
-		p.addButton("1. Numbered")
-		p.addButton(defaultChecklist + " Checklist")
-		var didSelect = p.show()
-		
-		if (didSelect) {
-			bullet = p.buttonPressed.match(/^.+(?= [a-z]+$)/i)[0]  // Extract bullet from selection
-			if (bullet == "1.") { bullet = "0." }  // Set up for addBullet() function
-			
-			// Create draft with given title
-			var d = Draft.create()
-			d.content = "# " + title + "\n"
-			d.addTag(listTag)
-			d.update()
-			return d
-		}
-		else {
-			return false
-		}
+		// Create draft with given title
+		var d = Draft.create()
+		d.content = "# " + p.fieldValues["title"] + "\n"
+		d.addTag(listTag)
+		d.update()
+		return d
+	}
+	else {
+		return false
+	}
+}
+
+var selectListType = function() {	
+	// Prompt for list style
+	p = Prompt.create()
+	p.title = "Select List Type"
+	p.addButton(defaultBullet + " Bulleted")
+	p.addButton("1. Numbered")
+	p.addButton(defaultChecklist + " Checklist")
+	var didSelect = p.show()
+	
+	if (didSelect) {
+		var b = p.buttonPressed.match(/^.+(?= [a-z]+$)/i)[0]  // Extract bullet from selection
+		if (b == "1.") { b = "0." }  // Set up for addBullet() function
+		return b
 	}
 	else {
 		return false
@@ -97,4 +80,30 @@ var addBullet = function(text, index) {
 	else {
 		return bullet.replace("[x]","[ ]") + " " + text.trim()  // Replace checked box with empty box
 	}
+}
+
+const bulletRegex = /^([-*+]( \[( |x)\])?|\d+\.)/gm
+var bullet = ""
+var d = selectDraft()
+
+if (d) {
+	var matches = d.content.match(bulletRegex)
+	if (matches) {  // No bullets found
+		bullet = matches.reverse()[0]
+	}
+	else {
+		bullet = selectListType()
+	}
+	console.log(bullet)
+	if (bullet) {
+		var lines = draft.content.replace(bulletRegex,"").split("\n")  // Strip existing bullets and split to array
+		d.content += "\n" + lines.map(addBullet).join("\n")  // Add bullet and append to draft
+		d.update()
+	}
+	else {
+		context.cancel()
+	}
+}
+else {
+	context.cancel()
 }
